2e5047f320b33e82874808ccd14182801fe8303f
wushengyong-forwork <375228075@qq.com>
2017-07-29 16:58:50 +0800

    1. Fixed the connect/close bugs. 2. add exec Function 3.Rebuild diretory.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include <node.h>
 #include "sqlite3cipherdb.h"
-#include <iostream>
-
-using namespace std;
 
 namespace sqlite3_cipher
 {
@@ -10,7 +7,6 @@ using namespace v8;
 
 void connect(const FunctionCallbackInfo<Value>& args)
 {
-	cout << "connect..." << endl;
 	Sqlite3CipherDb::connect(args);
 }
 
--- src/sqlite3cipherdb.h
+++ src/sqlite3cipherdb.h
@@ -13,29 +13,33 @@ using namespace v8;
 
 class Sqlite3CipherDb;
 
+class ThreadFunctions
+{
+public:
+	static void thread_ing(uv_work_t* req);
+	static void thread_done(uv_work_t* req, int status);
+};
 class Sqlite3CipherStmt : public node::ObjectWrap
 {
 public:
 	static void Init();
-
-	static Local<Object> prepare(Local<Object> db, Local<String> sql);
-
+	
 private:
-	explicit Sqlite3CipherStmt(Sqlite3CipherDb* pDB, std::string sql);
+	explicit Sqlite3CipherStmt();
 	~Sqlite3CipherStmt();
 
 	static void New(const FunctionCallbackInfo<Value>& args);
-
-	// �󶨲���
 	static void bind(const FunctionCallbackInfo<Value>& args);
-	// ִ��
+
 	static void exec(const FunctionCallbackInfo<Value>& args);
 
 	static Persistent<Function> constructor;
 
 	std::string           m_strSql;
 	sqlite3_stmt*         m_pStmts;
-	Sqlite3CipherDb*      m_pDB;
+	sqlite3*              m_pDB;
+
+	friend ThreadFunctions;
 };
 
 class Sqlite3CipherDb : public node::ObjectWrap
@@ -47,17 +51,16 @@ public:
 private:
     explicit Sqlite3CipherDb();
     ~Sqlite3CipherDb();
-
-    static void New(const FunctionCallbackInfo<Value>& args);
-
-
+	
+	static void New(const FunctionCallbackInfo<Value>& args);
     static void exec(const FunctionCallbackInfo<Value>& args);
     static void close(const FunctionCallbackInfo<Value>& args);
 	static void prepare(const FunctionCallbackInfo<Value>& args);
 
-public:
+private:
     static Persistent<Function> constructor;
     sqlite3* m_pDB; 
+	friend ThreadFunctions;
 };
 
 }
