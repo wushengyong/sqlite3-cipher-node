2c564e0ac1d5c2047420f4b90f885b0a4634e25f
wushengyong <375228075@qq.com>
2017-07-29 20:38:41 +0800

    1. writing tests.

---
+++ test/config.js
@@ -0,0 +1,5 @@
+var config = {
+    dbPath : "test.db",
+    passwd : "wushengyong"
+};
+module.exports = config;
---
+++ test/test.create.table.test.js
@@ -0,0 +1,20 @@
+var sqlite = require('../src/build/Release/sqlite3-cipher');
+var config = require('./config');
+
+sqlite.connect(config.dbPath, config.passwd,function (err, db){
+    if (err){
+        console.log(err);
+    }else{
+        db.exec("CREATE TABLE test ( intField INTEGER, int64Field INTEGER, doubleField DOUBLE, stringField TEXT, blobField BLOB);", function (err)
+        {
+            if (err){
+                console.log(err);
+            }
+            else {
+                console.log("create table test.");
+            }
+            db.close();
+        });
+    };
+    console.log("Create db : " + config.dbPath + " with key : " + config.passwd);
+})
